72732f7a607961a01892df67029b30998420667f
shourovahmd <moh.abuhanifa@gmail.com>
2023-06-18 22:18:36 +0600

    server setup

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
