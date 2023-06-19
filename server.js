dc7d11bc2e1cbb5fc43b75492465bc6e85663a3c
shourovahmd <moh.abuhanifa@gmail.com>
2023-06-19 10:53:17 +0600

    mongoose added

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.use(express.json());
 
 const port = process.env.PORT || 3000;
 
+app.get('/', (req, res) => {
+    res.send('Hello World');
+})
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 })
