f6f9db6a485517f8082c9ab3f6e3c710c02e035e
shourovahmd <moh.abuhanifa@gmail.com>
2023-06-19 12:44:43 +0600

    first user created using admin credentials

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,18 +5,25 @@ const User = require('../models/User');
 const registerUser = async (req, res) => {
     const { username, password } = req.body;
     try {
-        const hashed = bcrypt.hashSync(password, 10);
-        // const result = await User.create({
-        //     userName: username,
-        //     password: hashed,
-        //     roles: {
-        //         Admin: true
-        //     }
+        
+        const duplicate = await User.findOne({ userName: username });
+        if (duplicate) {
+            return res.json({
+                message: `User with username ${username} already exists`
+            })
+        } 
+        const hashed = await bcrypt.hashSync(password, 10);
+        const result = await User.create({
+            userName: username,
+            password: hashed,
+            roles: {
+                Admin: false
+            }
 
-        // })
+        })
         res.json({
             message: "hello from createUser",
-            data: hashed
+            data: result
         })
     } catch (error) {
 
--- middlewares/verifyAdmin.js
+++ middlewares/verifyAdmin.js
@@ -2,7 +2,7 @@ const User = require("../models/User")
 
 const verifyAdmin = async (req, res, next) => {
     const user = await User.findOne({
-        userName: req.body.username
+        userName: req.body.user
     }).exec()
 
     if (user?.roles?.Admin) {
