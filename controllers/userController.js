926e939297109070929f466c8370f373e95e6f4b
shourovahmd <moh.abuhanifa@gmail.com>
2023-06-19 12:05:47 +0600

    first user created and stored

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,22 @@
+const bcrypt = require('bcrypt');
+const User = require('../models/User');
+
+
 const registerUser = async (req, res) => {
-    console.log(req.body);
+    const { username, password } = req.body;
     try {
+        const hashed = bcrypt.hashSync(password, 10);
+        const result = await User.create({
+            userName: username,
+            password: hashed,
+            roles: {
+                Admin: true
+            }
+
+        })
         res.json({
-            message: "hello from createUser"
+            message: "hello from createUser",
+            data: result
         })
     } catch (error) {
 
