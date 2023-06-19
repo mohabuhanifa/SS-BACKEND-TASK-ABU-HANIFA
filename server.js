d0fb1e7f40d72ee52f9390e81d95ddda86259f7c
shourovahmd <moh.abuhanifa@gmail.com>
2023-06-19 11:08:51 +0600

    database connected

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const cors = require('cors');
+const connectDB = require('./config/db');
 const app = express();
 app.use(cors());
 app.use(express.json());
+require('dotenv').config();
+
+connectDB();
 
 const port = process.env.PORT || 3000;
 
