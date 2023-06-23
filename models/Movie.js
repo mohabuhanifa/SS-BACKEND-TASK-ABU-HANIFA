09a3d574842d74d47dfe2735970cc58dade5db44
shourovahmd <moh.abuhanifa@gmail.com>
2023-06-23 23:47:08 +0600

    Movie schema updated

--- models/Movie.js
+++ models/Movie.js
@@ -1,14 +1,30 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const MovieSchema = new Schema({
-  name: { type: String, required: true },
-  actors: [{ type: String }],
-  crew: [{ type: String }],
-  runtime: { type: Number, required: true },
-  directors: [{ type: String }],
-  producers: [{ type: String }],
-});
+const MovieSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+
+    actors: [{ type: String }],
+
+    crew: [{ type: String }],
+
+    runtime: {
+      type: Number,
+      required: true,
+    },
+
+    directors: [{ type: String }],
+
+    producers: [{ type: String }],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const Movie = mongoose.model("Movie", MovieSchema);
 
